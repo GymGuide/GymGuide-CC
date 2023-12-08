b7515d68d2f35cbbe59fddc1052c1e2a2521a548
m1tsuha4 <114287774+m1tsuha4@users.noreply.github.com>
2023-12-08 09:52:56 +0700

    exercise

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exercises = require('./Exercise.json'); // Import data latihan dari file JSON
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Route to retrieve exercises based on equipment
+app.get('/exercises', (req, res) => {
+    const { equipment } = req.query;
+
+    // Cari latihan yang memiliki equipment sesuai dengan query parameter
+    const filteredExercises = exercises.filter(exercise => exercise.equipment === equipment);
+
+    if (filteredExercises.length > 0) {
+        return res.json({
+            data: filteredExercises,
+            status: {
+                code: 200,
+                message: 'Success retrieving exercises'
+            }
+        });
+    } else {
+        return res.status(404).json({
+            status: {
+                code: 404,
+                message: 'Exercises not found for the specified equipment'
+            }
+        });
+    }
+});
+
+// Route to retrieve all exercises
+app.get('/all-exercises', (req, res) => {
+    return res.json({
+        data: exercises,
+        status: {
+            code: 200,
+            message: 'Success retrieving all exercises'
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
